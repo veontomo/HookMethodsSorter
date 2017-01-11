09315992920ae978c04bf1b8c305a420c40030d2
Andrey Shcherbakov <veontomo@gmail.com>
2017-01-11 12:58:06 +0100

    feat: stub the first action

---
+++ src/com/veontomo/hookmethodssorter/Sort.java
@@ -0,0 +1,15 @@
+package com.veontomo.hookmethodssorter;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * Sort well-known methods (hook ones).
+ */
+public class Sort extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
