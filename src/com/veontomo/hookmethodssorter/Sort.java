efda22e7825eddbea6953df6ae09150869b9125e
Andrey Shcherbakov <veontomo@gmail.com>
2017-01-16 09:22:54 +0100

    feat: stub iteration over all classes found in a file

--- src/com/veontomo/hookmethodssorter/Sort.java
+++ src/com/veontomo/hookmethodssorter/Sort.java
@@ -3,12 +3,9 @@ package com.veontomo.hookmethodssorter;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.LangDataKeys;
-import com.intellij.openapi.actionSystem.PlatformDataKeys;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.project.Project;
+import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.psi.*;
-import com.intellij.psi.util.PsiTreeUtil;
 
 /**
  * Sort well-known methods (hook ones).
@@ -17,19 +14,52 @@ public class Sort extends AnAction {
 
     @Override
     public void actionPerformed(AnActionEvent e) {
-        Project project = e.getData(PlatformDataKeys.PROJECT);
-        PsiFile psiFile = e.getData(LangDataKeys.PSI_FILE);
-        Editor editor = e.getData(PlatformDataKeys.EDITOR);
-
-        if (psiFile == null || editor == null) {
-            return;
+        PsiClass[] psiClasses = getPsiClasses(e);
+        if (psiClasses != null) {
+            elaborateMultipleClasses(psiClasses);
+        } else {
+            System.out.println("no class is found");
         }
 
-        int offSet = editor.getCaretModel().getOffset();
-        PsiElement elementAt = psiFile.findElementAt(offSet);
-        PsiClass psiClass = PsiTreeUtil.getParentOfType(elementAt, PsiClass.class);
-        String msg = psiClass == null ? "no class found" :  String.valueOf(psiClass.getMethods().length);
-        Messages.showMessageDialog(project, msg, "Information", Messages.getInformationIcon());
+    }
+
+    /**
+     * Iterate over given array of classes and elaborate each class.
+     *
+     * @param classes array of classes
+     */
+    private void elaborateMultipleClasses(final PsiClass[] classes) {
+        for (PsiClass aClass : classes) {
+            elaborateSingleClass(aClass);
+        }
+    }
+
+    /**
+     * Order the class methods in a predefined way.
+     *
+     * @param aClass a class whose methods/properties are to be ordered.
+     */
+    private void elaborateSingleClass(final PsiClass aClass) {
+        new WriteCommandAction.Simple(aClass.getProject(), aClass.getContainingFile()) {
+            @Override
+            protected void run() throws Throwable {
+                Messages.showMessageDialog(aClass.getProject(), "Sorting class " + aClass.getName(), "Info", Messages.getInformationIcon());
+            }
+        }.execute();
+    }
+
+
+    /**
+     * @param e the action event that occurred
+     * @return The PSIClass object based on which class your mouse cursor was in
+     */
+    private PsiClass[] getPsiClasses(AnActionEvent e) {
+        PsiFile psiFile = e.getData(LangDataKeys.PSI_FILE);
+        PsiClassOwner owner = (PsiClassOwner) psiFile;
+        if (owner != null) {
+            return owner.getClasses();
+        }
+        return null;
 
     }
 }
