2f2f8f42919479f4a62d7738e77105731ae0fb8c
Andrey Shcherbakov <veontomo@gmail.com>
2017-01-19 16:00:50 +0100

    feat: order the methods

--- src/main/com/veontomo/rearrange/Sorter.java
+++ src/main/com/veontomo/rearrange/Sorter.java
@@ -3,6 +3,11 @@ package main.com.veontomo.rearrange;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiMethod;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Sorts the fields and methods of the class in a predefined order.
  * A class is said to be sorted canonically if its fields and methods are in this order:
@@ -15,10 +20,41 @@ public class Sorter {
     /**
      * Return a new array constructed from the elements of a given array by sorting them in a given order.
      * @param methods
-     * @param base
+     * @param order list of methods name. Require that the list contains no pair of equal strings.
      * @return
      */
-    public PsiElement[] sort(final PsiMethod[] methods, final String[] base) {
-        return new PsiElement[0];
+    public PsiElement[] sort(final PsiMethod[] methods, final String[] order) {
+        PsiElement[] result = new PsiElement[methods.length];
+        // create an index of methods. Some methods might have equal names.
+        Map<String, List<PsiMethod>> index = new HashMap<>();
+        for (PsiMethod method : methods){
+            String name = method.getName();
+            if (index.containsKey(name)){
+                index.get(name).add(method);
+            } else {
+                List<PsiMethod> list = new ArrayList<>();
+                list.add(method);
+                index.put(name, list);
+            }
+        }
+        // put the methods from the order at the beginning
+        int counter = 0;
+        for (String methodName : order){
+            if (index.containsKey(methodName)){
+                for(PsiMethod method: index.get(methodName)){
+                    result[counter] = method;
+                    counter++;
+                }
+                index.remove(methodName);
+            }
+        }
+        // put the remaining methods
+        for (String methodName : index.keySet()){
+            for (PsiMethod method : index.get(methodName)){
+                result[counter] = method;
+                counter++;
+            }
+        }
+        return result;
     }
 }
